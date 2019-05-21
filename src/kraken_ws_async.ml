50e990527abc0d25625b10c8bb194ed4273d6ed1
Vincent Bernardoff <vb@luminar.eu.org>
2019-05-21 15:28:27 +0200

    remove timeout checking, some optimization

--- src/kraken_ws_async.ml
+++ src/kraken_ws_async.ml
@@ -7,30 +7,22 @@ open Kraken_ws
 let url = Uri.make ~scheme:"https" ~host:"ws.kraken.com" ()
 let sandbox_url = Uri.make ~scheme:"https" ~host:"ws-sandbox.kraken.com" ()
 
-exception Timeout
-
 let with_connection ?(sandbox=false) f =
   let url = if sandbox then sandbox_url else url in
   Fastws_async.with_connection_ez url ~f:begin fun r w ->
     let c = Lazy.force Time_stamp_counter.calibrator in
     let stop = Deferred.any_unit [Pipe.closed r ; Pipe.closed w] in
     (* Calibration *)
-    Clock_ns.every ~stop
-      (Time_ns.Span.of_int_sec 60)
-      (fun () -> Time_stamp_counter.Calibrator.calibrate c) ;
+    Clock_ns.every ~stop (Time_ns.Span.of_int_sec 60) begin fun () ->
+      Time_stamp_counter.Calibrator.calibrate c
+    end ;
     let last_ts = ref (Time_stamp_counter.now ()) in
-    (* Every 10 seconds, check that we got a message in the latest 10
-       seconds. *)
-    Clock_ns.every ~stop
-      (Time_ns.Span.of_int_sec 10)
-      begin fun () ->
-        if Time_stamp_counter.(Span.to_ns ~calibrator:c (diff (now ()) !last_ts)) >
-           Int63.(of_int 10_000_000_000) then raise Timeout
-      end ;
-    let r = Pipe.map r ~f:begin fun msg ->
+    let r = Pipe.map' r ~f:begin fun msgq ->
         last_ts := Time_stamp_counter.now () ;
-        !last_ts,
-        Ezjsonm_encoding.destruct_safe encoding (Ezjsonm.from_string msg)
+        return @@ Queue.map msgq ~f:begin fun msg ->
+          !last_ts,
+          Ezjsonm_encoding.destruct_safe encoding (Ezjsonm.from_string msg)
+        end
       end in
     let ws_read, client_write = Pipe.create () in
     don't_wait_for @@
--- src/kraken_ws_async.mli
+++ src/kraken_ws_async.mli
@@ -3,12 +3,9 @@ open Async
 
 open Kraken_ws
 
-exception Timeout
-
 val with_connection :
   ?sandbox:bool ->
-  ((Time_stamp_counter.t * t) Pipe.Reader.t ->
-   t Pipe.Writer.t ->
+  ((Time_stamp_counter.t * t) Pipe.Reader.t -> t Pipe.Writer.t ->
    'a Deferred.t) -> 'a Deferred.t
 (** [with_connection ?sandbox f]. Will raise [Timeout] if no message
     is received from Kraken in a 10sec time span. *)
