85c57c7b32e5d58e9573671d759e4405a6092c4b
Vincent Bernardoff <vb@luminar.eu.org>
2020-01-17 13:00:37 +0100

    test: use envvar

--- test/test.ml
+++ test/test.ml
@@ -5,26 +5,15 @@ open Alcotest
 open Kraken
 open Kraken_rest
 
-module Cfg = struct
-  type cfg = {
-    key: string ;
-    secret: string ;
-    passphrase: string [@default ""];
-    quote: (string * int) list [@default []];
-  } [@@deriving sexp]
-
-  type t = (string * cfg) list [@@deriving sexp]
-end
-
-let default_cfg = Filename.concat (Option.value_exn (Sys.getenv "HOME")) ".virtu"
-let cfg =
-  List.Assoc.find_exn ~equal:String.equal
-    (Sexplib.Sexp.load_sexp_conv_exn default_cfg Cfg.t_of_sexp) "KRAKEN"
+let key, secret =
+  match String.split ~on:':' (Sys.getenv_exn "TOKEN_KRAKEN") with
+  | [key; secret] -> key, secret
+  | _ -> assert false
 
 let wrap_request ?(speed=`Quick) n service =
   let auth = {
-    Fastrest.key = cfg.Cfg.key ;
-    secret = Base64.decode_exn cfg.Cfg.secret ;
+    Fastrest.key ;
+    secret = Base64.decode_exn secret ;
     meta = [] ;
   } in
   Alcotest_async.test_case n speed begin fun () ->
@@ -76,9 +65,9 @@ let rest = [
   wrap_request "closed_orders" (closed_orders ~ofs:0 ()) ;
   wrap_request "trade_history" (trade_history 0) ;
   wrap_request "ledgers" (ledgers ()) ;
-  wrap_request "DepositMethodsEUR"  (deposit_methods ~asset:"EUR") ;
+  (* wrap_request "DepositMethodsEUR"  (deposit_methods ~asset:"EUR") ; *)
   wrap_request "DepositMethodsBTC"  (deposit_methods ~asset:"XBT") ;
-  wrap_request "DepositMethodsXTZ"  (deposit_methods ~asset:"XTZ") ;
+  (* wrap_request "DepositMethodsXTZ"  (deposit_methods ~asset:"XTZ") ; *)
   wrap_request "DepositAddresses"  (deposit_addresses ~asset:"XBT" ~meth:"Bitcoin") ;
   wrap_request "DepositStatusXBT"  (transfer_status ~asset:"XBT" ~meth:"Bitcoin" `Deposit) ;
   wrap_request "DepositStatusXTZ"  (transfer_status ~asset:"XTZ" ~meth:"XTZ" `Deposit) ;
